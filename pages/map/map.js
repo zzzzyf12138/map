b64c5c9f181f0f29f5f1389376e54add50ed0c44
zzzzyf12138 <1097162523@qq.com>
2024-11-06 18:25:51 +0800

    Update map page and replace map overlay image

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,7 +1,16 @@
 // 引入腾讯地图SDK
 const QQMapWX = require('../../utils/qqmap-wx-jssdk.js');
 const qqmapsdk = new QQMapWX({
-  key: 'B6LBZ-H5C6Q-ARC5A-4C3AG-PWCTZ-L4BM5'  // 在此处填入刚才获取的密钥
+  key: 'B6LBZ-H5C6Q-ARC5A-4C3AG-PWCTZ-L4BM5'  
+});
+qqmapsdk.reverseGeocoder({
+  location: { latitude: 22.591792, longitude: 113.972976 },
+  success: (res) => {
+    console.log('腾讯地图API调用成功:', res);
+  },
+  fail: (error) => {
+    console.error('腾讯地图API调用失败:', error);
+  }
 });
 wx.cloud.init({
   env: 'test1-6gemrifqd6c55be5'
@@ -13,17 +22,41 @@ const markersCollection = db.collection('markers');
 
 Page({
   data: {
+    mapLayerId: '671791c94409', // 自定义图层 ID
     longitude: 113.972976	, // 初始地图中心经度
     latitude: 22.591792, // 初始地图中心纬度
     markers: []
   },
 
   onLoad() {
+    this.mapCtx = wx.createMapContext('myMap'); // 获取地图上下文
+    // 添加图片覆盖层
+    this.addImageOverlay();
     // 页面加载时获取用户地理位置
     this.getUserLocation(); 
-    this.loadMarkers(); // 页面加载时调用加载标记点的方法
+    // 页面加载时调用加载标记点的方法
+    this.loadMarkers(); 
   },
-  // 加载标记点
+  // 添加图片覆盖层
+  addImageOverlay() {
+    this.mapCtx.addGroundOverlay({
+      id: 1,  // 图层ID
+      src: '../../utils/resources/手绘地图.png',  // 手绘地图的图片路径
+      bounds: {
+        // 左下角
+        southwest: { latitude: 22.5887, longitude: 113.9652 },
+        // 右上角
+        northeast: { latitude: 22.5945,  longitude: 113.9745}
+      },
+      opacity: 0.8,  // 设置透明度，根据需求调整
+      success: (res) => {
+        console.log('地图图片覆盖层添加成功:', res);
+      },
+      fail: (e) => {
+        console.error('地图图片覆盖层添加失败:', e);
+      }
+    });
+  },
   loadMarkers() {
     const that = this;
     markersCollection.get().then(res => {
@@ -44,6 +77,7 @@ Page({
       console.error('获取标记点失败:', err);
     });
   },
+
   // 获取用户位置
   getUserLocation() {
     const that = this;
